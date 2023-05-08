bddb7dc4cd31b3491bca410d86648d81d877904a
Bryan <132607890+BryanRamarokoto@users.noreply.github.com>
2023-05-08 21:27:23 +0300

    Update UtilisateurController.php

--- app/Http/Controllers/UtilisateurController.php
+++ app/Http/Controllers/UtilisateurController.php
@@ -19,7 +19,7 @@ class UtilisateurController extends Controller
         $user = Utilisateur::where('email',$request->input('email'))->where('password',md5($request->input('password')));
         if($user->exists()) {
             session()->put('user',$user->first()->id);
-            return redirect()->route('Home');
+            return redirect()->route("Home");
         }
         else return redirect()->back()->with('Error','Email ou mot de passe incorrect!');
     }
